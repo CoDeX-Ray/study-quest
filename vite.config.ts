b6ebe2990afce6e040c1562e3416752f262ca1ac
CoDeX-Ray <raymundo3deborja@gmail.com>
2026-01-07 08:55:01 +0800

    vite ups 1

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/study-quest/',
+  base: '/study-quest',
   server: {
     host: "::",
     port: 8080,
