f15d0a1e03520b3d9c4c540b3ba1146d11330fee
CoDeX-Ray <raymundo3deborja@gmail.com>
2026-01-07 09:04:41 +0800

    router ups

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import { AuthProvider } from "./hooks/useAuth";
 
@@ -47,7 +47,7 @@ const LoadingFallback = () => (
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <BrowserRouter>
+      <HashRouter>
         <AuthProvider>
           <Toaster />
           <Sonner />
@@ -77,7 +77,7 @@ const App = () => (
             </Suspense>
           </div>
         </AuthProvider>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
